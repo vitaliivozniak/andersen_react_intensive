6f661ce714c36ef8fd6e1cbd46930ce5a3279f7c
Vitalii Vozniak <vitaliivozniak97@gmail.com>
2023-05-21 13:33:56 +0200

    [20.05]:finish. second  hw(add. logic)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,20 +1,6 @@
 import React, { Component } from 'react';
 import styles from './Button.module.css';
 
-// class Buttons extends Component {
-// 	constructor(props) {
-// 		super(props)
-// 	}
-
-// 	render() {
-// 		return (
-// 			<div className={styles.buttons}>
-// 				<button type="button" name="reset">Отмена</button>
-// 				<button type="submit" name="submit" onClick={this.props.getResult}>Сохранить</button>
-// 			</div>
-// 		)
-// 	}
-// }
 
 class Button extends Component {
 	constructor(props) {
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,20 +2,6 @@ import React, { Component } from 'react';
 import styles from './Input.module.css';
 
 
-// class Input extends Component {
-// 	constructor(props) {
-// 		super(props)
-// 	}
-
-// 	render() {
-// 		return (
-// 			<label>
-// 				{this.props.title}
-// 				<input type="text" name={this.props.name} placeholder={this.props.title} onChange={this.props.changeInputValue} />
-// 			</label>
-// 		)
-// 	}
-// }
 
 class Input extends Component {
 	constructor(props) {
